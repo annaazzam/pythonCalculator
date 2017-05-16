8b2bd5edc36f025d4b254850a16364ef03d155a7
Anna Azzam <annaazzam@live.com>
2017-05-16 16:51:18 +1000

    adding brackets and exponent functionality

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import re
+
 def printCalculator(expr):
 	print(calculator(expr))
 
@@ -11,5 +13,30 @@ def printCalculator(expr):
 #  - Addition: '+' character
 #  - Subtraction: '-' character
 def calculator(expr):
-	m = re.search(r'(\([^()]*\))', 'abcdef')
-	m.group(0)
+	# Remove spaces:
+	expr = re.sub(r'\s', '', expr)
+
+	# Add implicit multiplication signs
+	#  e.g. 5(1+2) ----> 5*(1+2)
+	m = re.search('([^\^\*\-\+\/])(\()', expr)
+	while (m.group()):
+		match = m.group(0) + m.group(1)
+		matchWithMult = m.group(0) + '*' + m.group(1)
+		expr = expr.replace(match, matchWithMult)
+		m = re.search('([^\^\*\-\+\/])(\()', expr)
+
+	# Evaluate Brackets:
+	m = re.search('(\([^()]*\))', expr)
+	for i in range(0, m.group().length):
+		withoutBrackets = re.sub('[\(\)]', '', m.group(i))
+		evaluatedBrackets = calculator(withoutBrackets)
+		expr = expr.replace(m.group(i), evaluatedBrackets)
+	
+	# Evaluate exponents:
+	# Regex search for (not-operator)^(not-operator)
+	m = re.search('([^\^\*\-\+\/]+)(\^)([^\^\*\-\+\/]+)', expr)
+	while (m.group()):
+		powerResult = power()
+	m = re.search('()\^()', expr)
+
+	return expr
