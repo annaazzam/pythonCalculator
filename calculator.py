d009babed7aa21be84424dd8cd2b648dbe93c01f
Anna Azzam <annaazzam@live.com>
2017-05-16 19:12:54 +1000

    addition / subtraction

--- calculator.py
+++ calculator.py
@@ -13,7 +13,7 @@ def printCalculator(expr):
 #  - Addition: '+' character
 #  - Subtraction: '-' character
 def calculator(expr):
-	# Remove spaces:
+	# Remove spaces for regex convenience
 	expr = re.sub(r'\s', '', expr)
 
 	# Add implicit multiplication signs
@@ -27,7 +27,7 @@ def calculator(expr):
 
 	# Evaluate Brackets:
 	m = re.search('(\([^()]*\))', expr)
-	for i in range(0, m.group().length):
+	for i in range(0, len(m.group())):
 		withoutBrackets = re.sub('[\(\)]', '', m.group(i))
 		evaluatedBrackets = calculator(withoutBrackets)
 		expr = expr.replace(m.group(i), evaluatedBrackets)
@@ -36,7 +36,30 @@ def calculator(expr):
 	# Regex search for (not-operator)^(not-operator)
 	m = re.search('([^\^\*\-\+\/]+)(\^)([^\^\*\-\+\/]+)', expr)
 	while (m.group()):
-		powerResult = power()
-	m = re.search('()\^()', expr)
+		match = m.group(0) + '^' + m.group(1)
+		lhs = m.group(0)
+		rhs = m.group(1)
+		powerResult = power(lhs, rhs)
+		expr = expr.replace(match, powerResult)
+		m = re.search('([^\^\*\-\+\/]+)(\^)([^\^\*\-\+\/]+)', expr)
 
-	return expr
+	# Evaluate multiplication:
+
+	# Evaluate division:
+
+	# Handle: negative numbers!! ?? ---- +- ---> -
+	# - at the beginning - -> negative number
+
+	# Evaluate addition / subtraction:
+	# SPLIT INTO TOKENS where a token is a number, or an operator (alternating)
+
+	result = 0
+	i = 0
+	while (i < len(tokens)):
+		if (tokens[i+1] == '+'):
+			result -= tokens[i]
+		elif (tokens[i+1] == '-'):
+			result += tokens[i]
+		i+=2
+
+	return result
